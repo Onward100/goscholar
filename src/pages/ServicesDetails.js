fdb07564384ec8d4f3f814b3458d9d4b3c4d96d1
woruka onward <worukaonward@gmail.com>
2024-06-18 18:46:10 +0100

    commit change

--- src/pages/ServicesDetails.js
+++ src/pages/ServicesDetails.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import servicesDetails from "./servicesData";
+import {servicesDetails} from "./servicesData";
 import { Link } from "react-router-dom";
 
 const ServicesDetails = () => {
@@ -8,6 +8,8 @@ const ServicesDetails = () => {
   const service = servicesDetails.find(
     (service) => service.id === parseInt(id, 10)
   );
+  console.log( id);
+console.log( service);
 
   if (!service) {
     return <div>Service not found</div>;
@@ -19,7 +21,8 @@ const ServicesDetails = () => {
         <h1>{service.name}</h1>
         <img src={service.image} alt={service.name} />
         <p>{service.description}</p>
-        <Link to="/services" className="services--btn">
+
+        <Link to="/" className="services--btn">
           Go back
         </Link>
       </div>
--- src/pages/servicesData.js
+++ src/pages/servicesData.js
@@ -55,64 +55,56 @@ const servicesDetails = [
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 2,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 3,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 4,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 5,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 6,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 7,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
   {
     id: 8,
     name: "Web Development",
     image:
       "https://images.pexels.com/photos/1595385/pexels-photo-1595385.jpeg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor",
   },
 ];
 
